4362d210bfa800b6c21adac55b8e083b8c7edded
shu1r0 <shimatani180231n@gmail.com>
2021-05-25 13:17:23 +0900

    update

--- pyshark_to_dict.py
+++ pyshark_to_dict.py
@@ -1,6 +1,10 @@
 import pyshark
 import asyncio
-import pprint
+import datetime
+import time
+from logging import getLogger
+from scapy.all import wrpcap
+from pyshark.packet.packet import Packet
 
 
 class PacketCapture:
@@ -12,39 +16,50 @@ class PacketCapture:
         capture (pyshark.LiveCapture)
     """
 
-    def __init__(self, interface):
+    def __init__(self, interface, output_file):
         """init
 
         Args:
             interface (str) : interface name
+            output_file (str) : output pcap file name
         """
         self.interface = interface
         self.event_loop = asyncio.get_event_loop()
-        self.capture = pyshark.LiveCapture(interface=self.interface, use_json=True)
+        self._output_file = output_file
+        self.capture = pyshark.LiveCapture(interface=self.interface, eventloop=self.event_loop, use_json=True, include_raw=True)
 
     def start(self, packet_count=None):
-        """packet capture start"""
+        """packet capture run"""
         try:
-            coroutine = self.capture.packets_from_tshark(self.packet_callback, packet_count=packet_count)
+            coroutine = self.get_write_pcap_coro(packet_count)
             self.event_loop.run_until_complete(coroutine)
         except KeyboardInterrupt as e:
             print("finish")
             exit()
 
-    def packet_callback(self, pkt):
-        """print packet
+    def get_write_pcap_coro(self, packet_count):
+        return self.capture.packets_from_tshark(self.write_pcap, packet_count=packet_count)
+
+    def close(self):
+        self.capture.close()
+
+    def write_pcap(self, pkt, do_print=True):
+        """write pcap file
 
         Args:
-            pkt (pyshark.packet.packet) : packet
+            pkt (Packet) : packet
+            do_print (bool) : do print?
         """
-        pprint.pprint({pkt.sniff_timestamp: packet_to_dict(pkt)})
+        if do_print:
+            print({pkt.sniff_timestamp: packet_to_dict(pkt)})
+        wrpcap(self._output_file, pkt.get_raw_packet(), append=True)
 
 
 def packet_to_dict(pkt):
     """packet to dict
 
     Args:
-        pkt (pyshark.packet.packet): packet
+        pkt (Packet): packet
     """
     dict_fields = {}
     for layer in pkt.layers:
@@ -56,7 +71,7 @@ def layer_to_dict(obj):
     """layer object to dict
 
     Args:
-        obj (pyshark.packet.layer) : packet layer
+        obj (pyshark.packet.layer.Layer) : packet layer
 
     Returns:
         dict
@@ -82,5 +97,6 @@ def layer_to_dict(obj):
 
 
 if __name__ == '__main__':
-    capture = PacketCapture("en0")
-    capture.start(packet_count=10)
+    output_file = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S.pcap')
+    capture = PacketCapture("en0", output_file)
+    capture.start()
